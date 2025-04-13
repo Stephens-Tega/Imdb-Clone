70d840fea1df688753462517f9c94c02c19796f0
Stephens Tega <tega@mypayledger.io>
2025-04-13 10:44:47 +0100

    create the navbar components

---
+++ src/app/about/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+        <h1 className="text-2xl font-medium text-amber-600">About</h1>
+        <p>Welcome to our movie database, your go-to destination for discovering movies, TV shows, and celebrities. Whether you're tracking award-winning films or debating the best action scenes of all time, we’ve got the details, ratings, and trivia to fuel your cinephile obsession.</p>
+
+        <p>Our database is a living, breathing film archive. Updated around the clock, it covers over 10 million titles across 100+ years of cinema history. Whether you’re researching that obscure indie film, fact-checking a movie debate with friends, or just looking for something to watch tonight, we’ve got you covered. Every entry is verified by our team of dedicated cinephiles who’d rather argue about ‘director’s cuts’ than sleep.</p>
+
+        <p>This isn’t just a website—it’s a gathering spot for the most passionate film fans on the internet. Rate movies with brutal honesty, track your watch history like a proud completionist, and dive into discussions where opinions hit harder than John Wick’s punches. Our community features everything from casual viewers to hardcore film theorists, all united by one belief: that one perfect movie exists for every mood, genre, and questionable life decision. Ready to find yours?</p>
+    </div>
+  )
+}
