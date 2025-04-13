995864b3fbfbbaec75f682592c7ecf4574b54240
Stephens Tega <tega@mypayledger.io>
2025-04-13 11:35:15 +0100

    fetched data from tmdb api in the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,21 @@
-export default function home() {
+import Results from "@/components/Results";
+
+const API_KEY = process.env.API_KEY;
+
+export default async function home({searchParams}) {
+  const genre = searchParams.genre || 'fetchTrending';
+  const res = await fetch(`
+    https://api.themoviedb.org/3${
+      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&language=en-US&page=1`
+    );
+    const data = await res.json();
+    if (!res.ok) {
+      throw new Error('Failed to fetch data')
+    }
+    const results = data.results;
   return (
-    <div>home</div>
+    <div>
+      <Results results={results} />
+    </div>
   )
 }
